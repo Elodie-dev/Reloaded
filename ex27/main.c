08cc55f35521a34f2f4f635f673bb87d31c2486c
Elodie De Cola <ede-cola@bess-f1r5s13.clusters.42paris.fr>
2023-11-02 11:06:35 +0100

    don t know what to say at this point

--- ex27/main.c
+++ ex27/main.c
@@ -6,7 +6,7 @@
 /*   By: ede-cola <ede-cola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:09:05 by ede-cola          #+#    #+#             */
-/*   Updated: 2023/11/02 10:46:18 by ede-cola         ###   ########.fr       */
+/*   Updated: 2023/11/02 11:04:02 by ede-cola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,14 @@ int	main(int argc, char **argv)
 
 	fd = 0;
 	if (argc < 2)
-		write(1, "File name missing.", 18);
+		write(1, "File name missing.\n", 19);
 	else if (argc > 2)
-		write(1, "Too many arguments.", 19);
+		write(1, "Too many arguments.\n", 20);
 	else
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
-			write(1, "Cannot read file.", 17);
+			write(1, "Cannot read file.\n", 18);
 		ft_putfile(fd);
 		close(fd);
 	}
